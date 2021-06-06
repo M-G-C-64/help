95576bda74bb9be1fa966546ce41069c812e33a1
GANESH CHANDRA <77651439+M-G-C-64@users.noreply.github.com>
2021-06-06 16:13:25 +0530

    Create ModuleNotFound.py

---
+++ ModuleNotFound.py
@@ -0,0 +1,13 @@
+#if moudle not found
+
+
+import sys
+sys.path
+
+#copy the path that ends with site-packages
+
+#try this code in command prompt
+
+#pip install --target=C:\\Users\\anilm\\AppData\\Roaming\\Python\\Python38\\site-packages package_name
+
+
